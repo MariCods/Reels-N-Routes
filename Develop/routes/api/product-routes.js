1a03e5d1436985054df66d15036c902d0b554e7c
Mary Cods <maryam.selassie@gmail.com>
2023-04-27 15:42:30 -0700

    Route Edits

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -40,7 +40,7 @@ router.post('/', (req, res) => {
   */
  Product.bulkCreate([
   {
-    product_name: [],
+    product_name: "Basketball Shirt",
       price: 200.00,
       stock: 3,
       tagIds: [1, 2, 3, 4]
@@ -50,26 +50,26 @@ router.post('/', (req, res) => {
  })
  .catch((err) => res.json(err));
 }); 
-  Product.create(req.body)
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+  // Product.create(req.body)
+  //   .then((product) => {
+  //     // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+  //     if (req.body.tagIds.length) {
+  //       const productTagIdArr = req.body.tagIds.map((tag_id) => {
+  //         return {
+  //           product_id: product.id,
+  //           tag_id,
+  //         };
+  //       });
+  //       return ProductTag.bulkCreate(productTagIdArr);
+  //     }
+  //     // if no product tags, just respond
+  //     res.status(200).json(product);
+  //   })
+  //   .then((productTagIds) => res.status(200).json(productTagIds))
+  //   .catch((err) => {
+  //     console.log(err);
+  //     res.status(400).json(err);
+  //   });
 
 
 // update product
